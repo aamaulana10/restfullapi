1119f71caf0243bf63abde978a0369f8d094203f
Aa Maulana <aamaulanabola10@gmail.com>
2025-05-05 13:15:50 +0700

    feat(jurusan): using Api Response and Global Handler

--- src/main/java/com/aamaulana/restfullapi/jurusan/controller/JurusanController.java
+++ src/main/java/com/aamaulana/restfullapi/jurusan/controller/JurusanController.java
@@ -1,11 +1,10 @@
 package com.aamaulana.restfullapi.jurusan.controller;
 
+import com.aamaulana.restfullapi.common.response.ApiResponse;
 import com.aamaulana.restfullapi.jurusan.dto.JurusanRequestDTO;
 import com.aamaulana.restfullapi.jurusan.dto.JurusanResponseDTO;
 import com.aamaulana.restfullapi.jurusan.service.JurusanService;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,77 +18,41 @@ public class JurusanController {
     private JurusanService jurusanService;
 
     @GetMapping
-    public ResponseEntity<List<JurusanResponseDTO>> getAllJurusan() {
-        try {
-            return ResponseEntity.ok(jurusanService.getAllJurusan());
-        }catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+    public ResponseEntity<ApiResponse<List<JurusanResponseDTO>>> getAllJurusan() {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", jurusanService.getAllJurusan()));
     }
 
     @GetMapping("/id={id}")
-    public ResponseEntity<JurusanResponseDTO> getJurusanById(@PathVariable Long id) {
-        System.out.println("id ====> " + id);
-        try {
-            return ResponseEntity.ok(jurusanService.getJurusanById(id));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public ResponseEntity<ApiResponse<JurusanResponseDTO>> getJurusanById(@PathVariable Long id) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", jurusanService.getJurusanById(id)));
 
     }
 
     @GetMapping("/name={name}")
-    public ResponseEntity<JurusanResponseDTO> getJurusanByName(@PathVariable String name) {
-        System.out.println("name ====> " + name);
-        var lowerCaseQuery = name.toLowerCase();
-        try {
-            return ResponseEntity.ok(jurusanService.getJurusanByName(name));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+    public ResponseEntity<ApiResponse<JurusanResponseDTO>> getJurusanByName(@PathVariable String name) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", jurusanService.getJurusanByName(name)));
     }
 
     @GetMapping("/query={query}")
-    public ResponseEntity<List<JurusanResponseDTO>> getJurusanByQuery(@PathVariable String query) {
-        try {
-            return ResponseEntity.ok(jurusanService.getJurusanByQuery(query));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+    public ResponseEntity<ApiResponse<List<JurusanResponseDTO>>> getJurusanByQuery(@PathVariable String query) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", jurusanService.getJurusanByQuery(query)));
     }
 
     @PostMapping
-    public ResponseEntity<String> createJurusan(@RequestBody JurusanRequestDTO request) {
-        try {
-            jurusanService.createJurusan(request);
-            return ResponseEntity.ok("success");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public ResponseEntity<ApiResponse<String>> createJurusan(@RequestBody JurusanRequestDTO request) {
+        jurusanService.createJurusan(request);
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", request.toString()));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateJurusan(@PathVariable Long id, @RequestBody JurusanRequestDTO request) {
-        try {
-            jurusanService.updateJurusan(id, request);
-            return ResponseEntity.ok("success update jurusan with id " + id);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public ResponseEntity<ApiResponse<String>> updateJurusan(@PathVariable Long id, @RequestBody JurusanRequestDTO request) {
+        jurusanService.updateJurusan(id, request);
+        return ResponseEntity.ok(new ApiResponse<>(true, "success update jurusan with id" + id, request.toString()));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteJurusanById(@PathVariable Long id) {
-        try {
-            jurusanService.deleteJurusanById(id);
-            return ResponseEntity.ok("Success delete with id: " + id);
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete");
-        }
+    public ResponseEntity<ApiResponse<String>> deleteJurusanById(@PathVariable Long id) {
+        jurusanService.deleteJurusanById(id);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Success delete with id: " + id, null));
     }
 }
