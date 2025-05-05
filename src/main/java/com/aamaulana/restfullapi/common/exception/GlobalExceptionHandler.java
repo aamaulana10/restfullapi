ce730279aba8ee42f9a45d2947594e741113b384
Aa Maulana <aamaulanabola10@gmail.com>
2025-05-05 13:01:44 +0700

    feat(common): Global Response & Exception

---
+++ src/main/java/com/aamaulana/restfullapi/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.aamaulana.restfullapi.common.exception;
+
+import com.aamaulana.restfullapi.common.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                false,
+                ex.getMessage(),
+                LocalDateTime.now(),
+                request.getDescription(false).replace("uri=", "")
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
