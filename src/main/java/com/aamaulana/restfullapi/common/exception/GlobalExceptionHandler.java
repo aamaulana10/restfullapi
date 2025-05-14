bf9f6e8ffd889516e773463ad326238fd386a8b4
Aa Maulana <aamaulanabola10@gmail.com>
2025-05-14 13:51:39 +0700

    tidy up

--- src/main/java/com/aamaulana/restfullapi/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aamaulana/restfullapi/common/exception/GlobalExceptionHandler.java
@@ -12,29 +12,29 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
-//        Map<String, String> errors = new HashMap<>();
-//
-//        ex.getBindingResult().getFieldErrors().forEach(error ->
-//                errors.put(error.getField(), error.getDefaultMessage())
-//        );
-//
-//        ApiResponse<Map<String, String>> response = new ApiResponse<>(false, "Validation Error", errors);
-//        return ResponseEntity.badRequest().body(response);
-//    }
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
-//        ErrorResponse error = new ErrorResponse(
-//                false,
-//                ex.getMessage(),
-//                LocalDateTime.now(),
-//                request.getDescription(false).replace("uri=", "")
-//        );
-//        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-//}
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        ApiResponse<Map<String, String>> response = new ApiResponse<>(false, "Validation Error", errors);
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                false,
+                ex.getMessage(),
+                LocalDateTime.now(),
+                request.getDescription(false).replace("uri=", "")
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
